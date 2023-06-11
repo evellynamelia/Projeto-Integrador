750ff164337ddc62d48e031ad8a87e5fce01db28
Evellyn Amélia <128506800+evellynamelia@users.noreply.github.com>
2023-06-10 21:54:18 -0300

    Atualizar o Tela de Login PI.py

--- Tela de Login PI.py
+++ Tela de Login PI.py
@@ -1,29 +1,31 @@
-
 from tkinter import *
 import tkinter.messagebox as MessageBox
 from PIL import Image, ImageTk, ImageFont, ImageDraw
 
+# Comando para direcionar a Tela2, depois de apertar o botão "  Iniciar  "
 def tela2():
     Tela2.place_forget()
     Tela2.place(width=370, height=620)
     Botao.place_forget()
 
+# Comando para receber os dados de Campo de Usuário/Senha
 def cadastro():
     print(CampoUsuario.get().strip())
     print(CampoSenha.get().strip())
-    CampoUsuario.delete(0, 'end')
+    CampoUsuario.delete(0, 'end') # Comando pra apagar os dados depois de executado;
     CampoSenha.delete(0, 'end')
 
+# Comando para os Campo de Usuário/Senha
 def set_transparent_entry_style(entry):
     entry.configure(background=entry.cget("bg"))
     entry.configure(highlightbackground="white", highlightcolor="white", highlightthickness=1)
 
-# Criação Da Tela 
+# Criação Da Tela Principal 
 Tela = Tk()
 Tela.geometry("370x620")
 Tela.title("SoUn EasyGym")
 
-# Incluindo Imagem de Fundo + Incluindo Tela secundaria(Quadrado Transparente)
+# Incluindo Imagem de Fundo
 Fundo = Image.open("C:\\Users\\evell\\OneDrive\\Documentos\\CURSO DS (ETE)\\Visual Code Projetos\\PyThon\\Projeto PI\\Imagens\\SoUn Principal.png")
 Fundo = Fundo.resize((370, 620), Image.ANTIALIAS)
 FundoTk = ImageTk.PhotoImage(Fundo)
@@ -41,23 +43,26 @@ y = (AlturaTela - Fundo.height) // 2
 label_fundo = Label(Tela, image=FundoTk)
 label_fundo.place(x=x, y=y)
 
-# Criação Da Tela2
+# Criação Da Tela de Cadastro 
 Tela2 = Frame(Tela)
 Fundo2 = Image.open("C:\\Users\\evell\\OneDrive\\Documentos\\CURSO DS (ETE)\\Visual Code Projetos\\PyThon\\Projeto PI\\Imagens\\SoUn Cadastro.png")
 Fundo2 = Fundo2.resize((370, 620))
 Fundo2Tk = ImageTk.PhotoImage(Fundo2)
 label_fundo = Label(Tela2, image=Fundo2Tk)
 
+# Dimensões da Tela
 LarguraTela2 = 370
 AlturaTela2 = 620
 
+# Calcular as coordenadas 
 x = (LarguraTela2 - Fundo2.width) // 2
 y = (AlturaTela2 - Fundo2.height) // 2
 
+# Rótulo com a imagem 
 label_fundo = Label(Tela2, image=Fundo2Tk)
 label_fundo.place(x=x, y=y)
 
-#Botão Tela 1 Inicial
+#Botão Tela Principal/ Botton Inicial
 Botao = Button(text="  Iniciar  ", bg="#d7d6ea", fg="#57d543", font=("Arial Black", 11), command= tela2)
 Botao.place(x=150, y=430)
 
@@ -71,6 +76,7 @@ CampoSenha = Entry(Tela2, text="", font=(17), width=20, show="*")
 set_transparent_entry_style(CampoSenha)
 CampoSenha.place(x=125, y=300)
 
+# Botão Tela Cadastro/ Botton Cadastrar 
 BotaoCadastro = Button(Tela2, text="  Cadastrar ", bg="#d7d6ea", fg="#57d543", font=("Arial Black", 11), command= cadastro)
 BotaoCadastro.place(x=130, y=400)
 
